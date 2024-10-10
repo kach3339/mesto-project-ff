ce025957f9e05041ac24e928306129fe6302eaf8
Anastasiia Kachalova <a.tynyanskaya@mail.ru>
2024-10-11 00:22:40 +0400

    issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,26 @@
-const placeCardTemplate = document.querySelector('#card-template').content;
+const placeCardTemplate = document.querySelector('#card-template').content.querySelector('.places__item');
 
 const placesCardContainer = document.querySelector('.places__list');
-const placeCardElement = placeCardTemplate.querySelector('.places__item');
 
 function addCard(cardTitleValue, cardImageLinkValue) {
-  const placeCardElementCopy = placeCardElement.cloneNode(true);
-  const placeCardTitle = placeCardElementCopy.querySelector('.card__title');
-  const placeCardImage = placeCardElementCopy.querySelector('.card__image');
-  const placeCardButtonRemove = placeCardElementCopy.querySelector('.card__delete-button');
+  const placeCardElement = placeCardTemplate.cloneNode(true);
+  const placeCardTitle = placeCardElement.querySelector('.card__title');
+  const placeCardImage = placeCardElement.querySelector('.card__image');
+  const placeCardButtonRemove = placeCardElement.querySelector('.card__delete-button');
 
   placeCardTitle.textContent = cardTitleValue;
   placeCardImage.setAttribute('src', cardImageLinkValue);
   placeCardImage.setAttribute('alt', cardTitleValue);
   
   placeCardButtonRemove.addEventListener('click', function (){
-    deleteCard(placeCardElementCopy)
+    deleteCard(placeCardElement)
   });
   
-  placesCardContainer.append(placeCardElementCopy);
+  placesCardContainer.append(placeCardElement);
 }
 
-const deleteCard = (placeCardElement) => {
-  placeCardElement.remove();
+const deleteCard = (placeCardElementForDelete) => {
+  placeCardElementForDelete.remove();
 };
 
 initialCards.forEach((item) => {
