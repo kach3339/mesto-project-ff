9b90211dd77832b51c047b869b7a53afdfa60a3d
Anastasiia Kachalova <a.tynyanskaya@mail.ru>
2024-10-20 22:53:41 +0400

    setup webpack build post css

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,12 @@ module.exports = {
         // при обработке этих файлов нужно использовать
         // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
+          'postcss-loader']
       }
     ]
   },
